2c202054fcbf73f3bb8b8a15872e5923f0a064c5
kyungwon <leekyungwon98@gmail.com>
2023-01-01 21:48:21 -0500

    React Set Up

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Welcome to GiftShop</h1>
+    </Fragment>
+  );
+}
+
+export default App;
